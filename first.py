73f780ce2d3749653822bdc4558edd57dd0f1f0d
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-07-20 12:13:10 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,19 @@
+# # print("Namaste")
+
+# # name = "sankalp singh"
+# # age = 21
+
+# # print("My name is: ", name)
+# # print("My age is: ", age)
+
+# a=5
+# b=8
+# sum = a+b
+# print(sum)
+
+name =input("kya naam hai tera: ")
+print("swagat hai", name)
+
+# int("5")
+# val = float(input("enter some value: "))
+# print(type(val),val)
