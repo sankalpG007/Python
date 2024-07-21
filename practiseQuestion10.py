0b7cff49c16dbd463b3942fda92c5dbb789a2905
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-07-21 23:24:57 +0530

    Add files via upload

---
+++ practiseQuestion10.py
@@ -0,0 +1,10 @@
+movies=[]
+movie1=input("Enter name of first movie: ")
+movie2=input("Enter name of second movie: ")
+movie3=input("Enter name of third movie: ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
