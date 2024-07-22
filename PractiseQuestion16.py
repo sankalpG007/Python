dc1a2e044bc788f7261000f46498a405eddbf375
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-07-22 23:55:41 +0530

    Add files via upload

---
+++ PractiseQuestion16.py
@@ -0,0 +1,13 @@
+subjects={
+    
+}
+subject1=int(input("enter marks of first subject: "))
+subjects.update({"first" :subject1})
+
+subject2=int(input("enter marks of second subject: "))
+subjects.update({"second" :subject2})
+
+subject3=int(input("enter marks of third subject: "))
+subjects.update({"third" :subject3})
+
+print(subjects)
