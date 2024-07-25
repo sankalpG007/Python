9adeacee50719a79515efa12461cd9780010e086
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-07-25 22:58:33 +0530

    Add files via upload

---
+++ practiseQuestion32.py
@@ -0,0 +1,11 @@
+gym=["push","pull","legs"]
+games=["football","swimming","wrestling"]
+def  print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+
+print_list(gym)    
+print_list(games) 
